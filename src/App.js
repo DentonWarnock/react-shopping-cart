1e5fadc9dab41d1e321768d25af0111ea45245d7
DentonWarnock <dentonwarnock@gmail.com>
2019-11-13 15:12:38 -0800

    CartContext created and added to App.js. All props removed.

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import ShoppingCart from './components/ShoppingCart';
 
 // Context
 import {ProductContext} from './contexts/ProductContext';
+import {CartContext} from './contexts/CartContext';
 
 function App() {
 	const [products] = useState(data);
@@ -23,16 +24,15 @@ function App() {
 
 	return (
 		<ProductContext.Provider value={{ products, addItem }} >
-		<div className="App">
-			<Navigation cart={cart} />
-
-			{/* Routes */}
-			<Route exact path="/" component={Products} />
-			<Route path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
-			/>
-		</div>
-		</ ProductContext.Provider >
+			<CartContext.Provider value={{ cart }} >
+				<div className="App">
+					<Navigation />
+					{/* Routes */}
+					<Route exact path="/" component={Products} />
+					<Route path="/cart" component={ShoppingCart} />}					
+				</div>
+			</CartContext.Provider>
+		</ProductContext.Provider >
 	);
 }
 
